1d997a742edaa86f64e08fabc6bd8ef49b995b1c
Peter Englmaier <peter.englmaier@gmail.com>
2026-02-22 14:07:22 +0100

    Sync requirements-prod.txt with requirements.txt

--- docker-build.sh
+++ docker-build.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 # specify options if needed
+echo docker build -t vds-sternwarte:latest . $@
 docker build -t vds-sternwarte:latest . $@
+echo docker tag vds-sternwarte:latest penglmaier/vds-sternwarte:latest
 docker tag vds-sternwarte:latest penglmaier/vds-sternwarte:latest
--- docker-push.sh
+++ docker-push.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+echo docker push penglmaier/vds-sternwarte:latest
 docker push penglmaier/vds-sternwarte:latest
 if [ -n "$1" ]; then
+  echo docker tag penglmaier/vds-sternwarte penglmaier/vds-sternwarte:$1
   docker tag penglmaier/vds-sternwarte penglmaier/vds-sternwarte:$1
+  echo docker push penglmaier/vds-sternwarte:$1
   docker push penglmaier/vds-sternwarte:$1
 fi
